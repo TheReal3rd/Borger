19f2beb3e9cc8eee0055ae0bb7094487b59aee28
³ʳᵈ <60966545+TheReal3rd@users.noreply.github.com>
2023-05-26 22:14:57 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,8 +10,13 @@ forever(function () {
     mySprite.setPosition(PosX, PosY)
     if (PosX < 0 || PosX > scene.screenWidth()) {
         VelX = VelX * -1
+        info.changeScoreBy(1)
     }
     if (PosY < 0 || PosY > scene.screenHeight()) {
         VelY = VelY * -1
+        info.changeScoreBy(1)
+    }
+    if (PosX < 0 && PosY < 0 || PosX > scene.screenWidth() && PosY > scene.screenHeight() || (PosX > scene.screenWidth() && PosY < 0 || PosX < 0 && PosY > scene.screenHeight())) {
+        scene.setBackgroundColor(2)
     }
 })
